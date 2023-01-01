f99e91ab230087ff41c33128c2c233ab9755e253
MD Pahlovi <mdpahlovi07@gmail.com>
2023-01-01 09:09:20 +0600

    UseAuth unndifine issue

--- pages/about/[email].js
+++ pages/about/[email].js
@@ -8,6 +8,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { apiUrl } from "../../requests/url";
 import { CirclesWithBar } from "react-loader-spinner";
+import { useAuth } from "../../hooks/useAuthContext";
 
 const About = () => {
     const [user, setUser] = useState({});
